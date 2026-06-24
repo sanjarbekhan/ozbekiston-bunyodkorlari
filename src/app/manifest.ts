88f1aba81aa9de220a9596bdcfc017e4746295cb
sanjarbekhan <sanjarhasanov465@gmail.com>
2026-06-25 04:27:22 +0500

    Sayt manifest fayli qo'shildi

---
+++ src/app/manifest.ts
@@ -0,0 +1,26 @@
+﻿import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "O‘zbekiston Bunyodkor Yoshlari Ensiklopediyasi",
+    short_name: "O‘zBYE",
+    description:
+      "O‘zbekiston rivojiga munosib hissa qo‘shayotgan bunyodkor yoshlar ensiklopediyasi.",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#0043a4",
+    icons: [
+      {
+        src: "/icon.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "/apple-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+  };
+}
