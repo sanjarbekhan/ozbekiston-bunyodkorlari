318e8528b05db44725da4907b7dbad85d6e5f43c
Sanjarbek Xasanov <sanjarhasanov465@gmail.com>
2026-08-08 02:07:27 +0500

    Align site metadata with www primary host

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-const SITE_URL = "https://bunyodkor.com";
+const SITE_URL = "https://www.bunyodkor.com";
 
 export const metadata: Metadata = {
   metadataBase: new URL(SITE_URL),
