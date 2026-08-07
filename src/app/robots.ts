ab7c808f2fe74ef331536c09e4e3cdca5211b6ec
Sanjarbek Xasanov <sanjarhasanov465@gmail.com>
2026-08-08 02:07:53 +0500

    Align robots sitemap with www primary host

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from "next";
 
-const SITE_URL = "https://bunyodkor.com";
+const SITE_URL = "https://www.bunyodkor.com";
 
 export default function robots(): MetadataRoute.Robots {
   return {
